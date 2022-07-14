46e6838afa841d3df7376fea62632b476eecf920
Nathalia Alencastro <108465597+nathaliaalencastro@users.noreply.github.com>
2022-07-14 15:31:13 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,73 @@
+#destaques1{
+    background-color: rgb(126, 23, 126);
+height: 300px;
+width: 55%;
+margin-left: 5%;
+margin-right: 5%;
+    position: relative;
+}
+#titulo{
+    color: white;
+    margin-left: 21px;
+    margin-top: 90px;
+    padding-top: 22px;
+    font-family: 'Times New Roman', Times, serif;
+}
+#destaques{
+    background-color: rgba(204, 0, 204, 0.747);
+}
+#livro{
+    margin-left: 275px;
+    color: white;
+    font-size: 20px;
+}
+#paragrafo1{
+    margin-left: 215px;
+    color: white;
+    margin-right: 10px;
+    margin-top: 30px;
+}
+.botao{
+    border-radius: 20px;
+    background-color: white;
+    color: black;
+    font-size: 15px;
+    border: none;
+    float: right;
+    margin-right: 30px;
+  margin-top: 20px;
+}
+
+header{
+    background-color: rgb(95, 0, 0);
+    border: black;
+    height: 100px;
+    width: 100%;
+    margin-top: 30px;
+ 
+ 
+}
+ li{
+    display: inline-block;
+    text-decoration: none;
+    margin-top: 70px;
+    margin-left: 110px;
+    color: white;
+}
+button:hover{
+background-color: firebrick;
+font-family: monospace;
+}
+button{
+    font-family: monospace;
+    border: none;
+   font-size: 15px; 
+   background-color: rgb(80, 0, 0);
+   color: white;
+}
+
+*{
+    padding: 0;
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+}
